8add0d72747415b62f56be1f9d8357a9c20f2f92
watanabeaina <aina0721omiomi@yahoo.com>
2023-05-19 12:29:53 +0900

    Enemyを追尾するようにした

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        score = 0;
+        SetCountText();
+        winText.text = "";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            score = score + 1;//内部的にスコアを増やす
+
+            SetCountText();//ついでに、UI更新委託
+        }
+    }
+    void SetCountText()//UIの更新を委託
+    {
+        scoreText.text = "Count: " + score.ToString();
+
+        if (score >= 2)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
